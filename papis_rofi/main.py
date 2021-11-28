4f155fb8a4d01f1be73b03535046f7507dd4ad1e
Gorka Kobeaga <gorka@kobeaga.eus>
2021-11-28 14:31:27 +0100

    Fix edit command call

--- papis_rofi/main.py
+++ papis_rofi/main.py
@@ -165,7 +165,7 @@ class Gui(object):
                 indices = [indices]
             if key == self.edit_key:
                 for i in indices:
-                    edit(self.documents[i], editor=papis.config.get('editor'))
+                    edit(self.documents[i])
             elif key in [self.open_key, self.open_stay_key]:
                 docs = [self.documents[i] for i in indices]
                 if key == self.open_key:
